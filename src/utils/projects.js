eae556e36e2777c1689322e1a93f0b029b10c49f
ElianKas <135838274+ElianKas@users.noreply.github.com>
2025-08-10 17:39:14 +0200

    a

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -8,9 +8,9 @@ export const projects = [
 		cardClass: 'rotate-[-4.5deg] bg-primary',
 		pinClass: 'bg-[#8053ea] top-[-5px] left-[110px]',
 		description: [
-			'Ein interaktives Webprojekt, das die digitale Transformation in der Kunst- und Designausbildung thematisiert.',
-			'Die Seite bietet eine ansprechende Benutzeroberfläche mit Animationen und interaktiven Elementen, um die Inhalte lebendig zu präsentieren.',
-			'Entwickelt mit Nuxt, Tailwind CSS für das Styling und GSAP für die Animationen.',
+			'Ein sich ständig vergrößerndes Archiv der Universitäten Hessens, in dem Beiträge und Literatur zum Thema digitale Transformation in der Kunst- und dem Design gespeichert werden.',
+			'Die Webseite bietet Filter- und Suchmöglichkeiten für Nutzer und stellt Storyblok Inhalte dynamisch dar.',
+			'Entwickelt mit Nuxt, Tailwind und Gsap.',
 		],
 	},
 	{
@@ -25,7 +25,7 @@ export const projects = [
 		description: [
 			'Eine Community-Plattform für die Kunsthochschule Kassel, die es Studierenden ermöglicht, sich zu vernetzen und auszutauschen.',
 			'Die Plattform bietet Funktionen wie einen anonymen Live-Chat mit Like-Funktion und eine Uploadmaske für Studenten, um ihre Events zu teilen.',
-			'Entwickelt wurde die Plattform mit Nuxt und TypeScript. Für Events und Chat wurden Supabase tables und storage buckets eingesetzt. Sharp wird als Bild-Kompressor verwendet.',
+			'Entwickelt wurde die Plattform mit Nuxt und TypeScript. Für Events und Live-Chat wurden Supabase tables und storage buckets eingesetzt. Sharp wird als Bild-Kompressor verwendet.',
 		],
 	},
 	{
@@ -37,23 +37,23 @@ export const projects = [
 		cardClass: 'rotate-[-7deg] bg-accent',
 		pinClass: 'bg-[#EF5F1D] top-[-6px] right-[70px]',
 		description: [
-			'Ein Webprojekt, das sich für die Unterstützung von Menschen in Afrika einsetzt.',
-			'Die Seite bietet eine ansprechende Benutzeroberfläche mit Animationen und interaktiven Elementen, um die Inhalte lebendig zu präsentieren.',
-			'Entwickelt mit Nuxt, Storyblok für das Content-Management und Nodemailer für das E-Mail-Management.',
+			'Ein Webprojekt, das sich für die Unterstützung von Menschen in Afrika einsetzt und dem e.V. Miteinander für Menschen in Afrika bei der Spendensammlung hilft.',
+			'Die Webseite bietet eine umfangreiche CMS Anbindung für Standorteingaben der Brunnen in Afrika, dynamischen Storyseiten mit hoher Gestaltungsfreiheit, Videointegration, Newsletter und vielem mehr.',
+			'Entwickelt mit Nuxt, Storyblok CMS und Nodemailer für das E-Mail-Management.',
 		],
 	},
 	{
 		id: 'smartops',
 		title: 'SmartOPS',
-		tech: 'React | Node.js | MongoDB',
+		tech: 'Astro | Gsap | Tailwind',
 		website: 'https://www.smartops.de/',
 		imageSrc: '/images/www.smartops.de_.png',
-		cardClass: 'rotate-[2deg] bg-primary',
-		pinClass: 'bg-[#4a90e2] top-[-5px] left-[90px]',
+		cardClass: 'rotate-[0deg] bg-primary',
+		pinClass: 'bg-[#4a90e2] top-[-5px] right-[150px]',
 		description: [
-			'Eine intelligente Operations-Plattform für Unternehmen.',
-			'Bietet umfassende Tools für das Management von Geschäftsprozessen.',
-			'Entwickelt mit modernen Web-Technologien für optimale Performance.',
+			'Eine starke Online-Präsenz für die Energieberatungsfirma SmartOPS.',
+			'Derzeit in der Entwicklung ist ein zugehöriger Datencenter für die Einsicht von Energiedaten aus Deutschland, sowie für die Prognose von Energieverbrauchswerten.',
+			'Astro, Gsap und Tailwind sind für die Darstellung im Einsatz. Die SmardAPI und eigene Formeln zur Prognose werden für die Datenanbindung verwendet.',
 		],
 	},
 ];
